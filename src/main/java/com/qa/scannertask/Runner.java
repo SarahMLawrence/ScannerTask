2671f19c54f106a85ca6223c496148fc2dd328c7
Sarah Lawrence <sarahlawrence1152@gmail.com>
2021-05-25 15:57:36 -0400

    initial commit

---
+++ src/main/java/com/qa/scannertask/Runner.java
@@ -0,0 +1,75 @@
+package com.qa.scannertask;
+
+//-------//
+//IMPORTS//
+//-------//
+import java.util.Scanner;
+
+public class Runner {
+
+	// --------------//
+	// DECLARATIONS //
+	// --------------//
+	public static Scanner scanner = new Scanner(System.in);
+
+	// -------------//
+	// MAIN METHOD //
+	// -------------//
+	public static void main(String[] args) {
+		System.out.println("*****Welcome to Math Choice*****");
+
+		int result = 0;
+		boolean flag = true;
+
+		do {
+			System.out.println("Please Make Your Choice: " + "\n1. Addition" + "\n2: Subtraction" + "\n3. Multiply"
+					+ "\n4. Divide");
+
+			result = scanner.nextInt();
+			switch (result) {
+			case 1:
+				System.out.println(addOption(10, 5));
+				break;
+			case 2:
+				System.out.println(subOption(20, 2));
+				break;
+			case 3:
+				System.out.println(multOption(4, 5));
+				break;
+
+			case 4:
+				System.out.println(divOption(20, 10));
+				break;
+
+			case 0:
+				System.out.println("exiting app");
+				flag = false;
+				break;
+			default:
+
+				System.out.println("The number you entered is not one of the choices!");
+				flag = false;
+				break;
+			}
+		} while (flag);
+
+		scanner.close();
+
+	}
+
+	public static int addOption(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public static int subOption(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public static int multOption(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	public static int divOption(int num1, int num2) {
+		return num1 / num2;
+	}
+}
